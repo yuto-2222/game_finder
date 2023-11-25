e7d9b3731d5fc4dcb473c1050fec3849dbbfb803
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-11-25 09:52:16 +0000

    最終確認

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -29,34 +29,15 @@ class Public::SessionsController < Devise::SessionsController
 
   def user_state
     # 入力されたemailからアカウントを1件取得
-    @user = User.find_by(email: params[:user][:email])
-    # アカウントを取得できなかった場合、このメソッドを終了する
+    user = User.find_by(email: params[:user][:email])
+    # アカウントを取得できなかった場合、このメソッドを終了
+    return if user.nil?
     # 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-    if @user.valid_password?(params[:user][:password]) && !@user.is_active
+    # user.statusがfalseだったときに遷移させる
+    if user.valid_password?(params[:user][:password]) && !user.is_active
+      flash[:notice] = "This account has been deleted."
       redirect_to new_user_registration_path
     end
   end
 
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
