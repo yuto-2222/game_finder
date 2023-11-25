51c1081b49d247f67680b9b274dd3559b217c748
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-11-25 07:40:11 +0000

    最終確認

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -6,7 +6,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :guest_not_destroy, only: [:destroy]
 
   def after_sign_in_path_for(resource)
-    user_path(current_customer)
+    user_path(current_user)
   end
 
 
