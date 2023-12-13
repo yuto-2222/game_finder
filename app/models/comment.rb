d43b166de4a8725a932d9f92f73460ebe5ee7119
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-12-13 06:21:44 +0000

    comment非同期エラー修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,24 +1,26 @@
 class Comment < ApplicationRecord
 
 	# バリデーション
-  validates :content, presence: true
+	validates :content, presence: true
 
-  # アソシエーション
-  belongs_to :user
-  belongs_to :review
+	# アソシエーション
+	belongs_to :user
+	belongs_to :review
 
-  has_one :notification, as: :subject, dependent: :destroy
+	has_one :notification, as: :subject, dependent: :destroy
 
-  has_many :reports, foreign_key: "reported_id", as: :reported, dependent: :destroy
+	has_many :reports, foreign_key: "reported_id", as: :content, dependent: :destroy, class_name: "Report"
 
-  # 通知関連
-  after_create_commit :create_notifications
+	# 通知関連
+	after_create_commit :create_notifications
 
-  private
+	private
 
-  def create_notifications
-    # Notificationに、レビュー対してコメントがあった際に、userにactionされたことを残す
-    Notification.create(subject: self, user: review.user, action: :commented_to_own_review)
-  end
+	def create_notifications
+		# Notificationに、レビュー対してコメントがあった際に、userにactionされたことを残す
+		if self.review.user.nickname != user.nickname
+			Notification.create(subject: self, user: review.user, action: :commented_to_own_review)
+		end
+	end
 
 end
--- app/models/useful.rb
+++ app/models/useful.rb
@@ -1,19 +1,21 @@
 class Useful < ApplicationRecord
-	
+
 	# アソシエーション
 	belongs_to :user
 	belongs_to :review
-	
+
 	has_one :notification, as: :subject, dependent: :destroy
-	
+
 	# 通知関連
 	after_create_commit :create_notifications
 
-  private
-  
-  def create_notifications
-  	# Notificationに、レビュー対してusefulされた際に、userにactionされたことを残す
-    Notification.create(subject: self, user: self.review.user, action: :useful_to_own_review)
-  end
+	private
+
+	def create_notifications
+		# Notificationに、レビュー対してusefulされた際に、userにactionされたことを残す
+		if self.review.user.nickname != user.nickname
+			Notification.create(subject: self, user: self.review.user, action: :useful_to_own_review)
+		end
+	end
 
 end
